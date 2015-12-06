2d79fcc7e3165eb4b536493827235eb626a424fe
Marios Zoulias <t8140029@dias.aueb.gr>
2015-12-06 18:59:14 +0200

    Delete separator.java

--- separator.java
+++
@@ -1,8 +0,0 @@
- class separator{
-	  public static void main (String[] args){
-		  String s = "ABC[ This is  extract]";
-    	int startIndex = s.indexOf('[');
-    	int endIndex = s.indexOf(']');
-    	System.out.println(s.substring(startIndex + 1, endIndex));
-    }
-}
