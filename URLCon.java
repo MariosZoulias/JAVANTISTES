e100bafa6d83e626ade4d541dc009ddae51d36a4
kkaragiannakis <karagiannakisk@yahoo.gr>
2015-12-08 22:52:52 +0200

    Update URLCon.java

--- URLCon.java
+++ URLCon.java
@@ -1,33 +1,53 @@
+/**
+*Dimiourgia txt me ton kodika html mias selidas
+*Na bazete ollo to url ekei pou to zitaei "https://www.google.gr/"
+*
+*@author Karagiannakis kon., Mpatris St., Kokkinoplitis Nik., Fragkakis Pan.
+*@Version 1.0
+*@8/12/2015
+*/
+
 import java.net.*;
 import java.io.*;
 import java.util.*;
 import java.io.File;
 import java.io.BufferedWriter;
 
-public class URLCon {
+
+
+public class HtmlTxt {
     public static void main(String[] args) throws Exception {
 
         Scanner input = new Scanner(System.in);
+
+		String encoding = "UTF-8"; // Or "ISO-8859-7"
+
 
 
 		String string_url;
         System.out.println("enter the website: ");
         string_url = input.next();
 
-		URL a = new URL(string_url);
+		URL a = new URL("http://"+string_url);
 		URLConnection y_c = a.openConnection();
 		BufferedReader in = new BufferedReader(new InputStreamReader(
-                                    y_c.getInputStream()));
+                                    y_c.getInputStream(),encoding));
 
 		try{
-			File file = new File("C:/Users/User/Desktop/html.txt");
+			File file = new File("C:/Users/stamatis/Desktop/html.txt");
 
 			FileWriter fw = new FileWriter(file.getAbsoluteFile());
 			BufferedWriter bw = new BufferedWriter(fw);
+			StringBuffer sb = new StringBuffer();
+			String line = null;
 
 			String inputLine;
 			while ((inputLine = in.readLine()) != null){
-            	bw.write(inputLine);
+
+				bw.write(inputLine);
+            	bw.newLine();
+            	sb.append(line);
+
 			}
 			bw.close();
 		} catch (IOException e) {
