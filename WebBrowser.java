597d9f47dd603756ba4862f2f32f3103dbf00881
John Ladopoulos <johnny.lad96@gmail.com>
2015-12-11 16:23:32 +0200

    update webbrowser
    creates a string that consists of the url that the user types and passes it to mehod createTxt.

--- WebBrowser.java
+++ WebBrowser.java
@@ -1,5 +1,8 @@
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
@@ -9,7 +12,7 @@ import javax.swing.JScrollBar;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class WebBrowser extends JFrame {
+public class WebBrowser extends JFrame implements ActionListener {
 	
 	private JPanel middlePanel;
 	private JTextField textField;
@@ -24,17 +27,19 @@ public class WebBrowser extends JFrame {
 		frame.setTitle("Javantistes Web Browser");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(750, 400);
-		frame.setVisible(true);
-		
+		frame.setVisible(true);		
 	}
 	
 	public WebBrowser() {
 		middlePanel = new JPanel();
 		middlePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 	    middlePanel.add(label = new JLabel("URL:"));
+	    
 	    middlePanel.add(textField = new JTextField(40));
 	    textField.setToolTipText("Enter URL");
 	    getContentPane().add(middlePanel, BorderLayout.NORTH);
+	    
+	    textField.addActionListener(this);
         
 	    textArea = new JTextArea();
 	    textArea.setEditable(false);
@@ -45,6 +50,13 @@ public class WebBrowser extends JFrame {
 		
 	}
 	
+	public void actionPerformed(ActionEvent e) {
+		String stringUrl = textField.getText();
+		URLCon urlcon = new URLCon();
+		urlcon.createTxt(stringUrl);
+	} 
+	
 	
 	
 }
+
