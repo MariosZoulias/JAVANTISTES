c1360aa1cbc341d8c339c4f1626ac23793895b9f
John Ladopoulos <johnny.lad96@gmail.com>
2015-12-11 16:48:25 +0200

    update webbrowser 2

--- WebBrowser.java
+++ WebBrowser.java
@@ -13,16 +13,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class WebBrowser extends JFrame implements ActionListener {
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-
-=======
 	
->>>>>>> origin/master
->>>>>>> origin/master
->>>>>>> origin/master
 	private JPanel middlePanel;
 	private JTextField textField;
 	private JLabel label;
@@ -30,105 +21,41 @@ public class WebBrowser extends JFrame implements ActionListener {
 	private JEditorPane displayEditorPane;
 	private JTextArea textArea;
 	private JScrollBar scrollBar;
-
+	
 	public static void main(String[] args) {
 		WebBrowser frame = new WebBrowser();
 		frame.setTitle("Javantistes Web Browser");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(750, 400);
-		frame.setVisible(true);
-=======
-<<<<<<< HEAD
-		frame.setVisible(true);
-
-<<<<<<< HEAD
-		frame.setVisible(true);
-=======
-=======
 		frame.setVisible(true);		
->>>>>>> origin/master
->>>>>>> origin/master
->>>>>>> origin/master
 	}
-
+	
 	public WebBrowser() {
 		middlePanel = new JPanel();
 		middlePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 	    middlePanel.add(label = new JLabel("URL:"));
-
-=======
-<<<<<<< HEAD
-
-	    middlePanel.add(textField = new JTextField(40));
-	    textField.setToolTipText("Enter URL");
-	    getContentPane().add(middlePanel, BorderLayout.NORTH);
-
-<<<<<<< HEAD
-
-=======
-=======
 	    
->>>>>>> origin/master
->>>>>>> origin/master
 	    middlePanel.add(textField = new JTextField(40));
 	    textField.setToolTipText("Enter URL");
 	    getContentPane().add(middlePanel, BorderLayout.NORTH);
-
+	    
 	    textField.addActionListener(this);
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-
-=======
         
->>>>>>> origin/master
->>>>>>> origin/master
->>>>>>> origin/master
 	    textArea = new JTextArea();
 	    textArea.setEditable(false);
 	    getContentPane().add(textArea, BorderLayout.CENTER);
-
+	    
 	    scrollBar = new JScrollBar();
 	    getContentPane().add(scrollBar, BorderLayout.EAST);
+		
 	}
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-
-
-
-=======
 	
->>>>>>> origin/master
->>>>>>> origin/master
 	public void actionPerformed(ActionEvent e) {
 		String stringUrl = textField.getText();
 		URLCon urlcon = new URLCon();
-		try {
 		urlcon.createTxt(stringUrl);
-<<<<<<< HEAD
-	}
-=======
-<<<<<<< HEAD
-		} catch(Exception exc) {
-			System.out.println("Exception !");
-		  }
-	}
-
-
-
-=======
 	} 
 	
 	
 	
->>>>>>> origin/master
->>>>>>> origin/master
->>>>>>> origin/master
 }
-
