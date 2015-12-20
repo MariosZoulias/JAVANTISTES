581c2056019ecd6787812c5e82028fca4b777cf3
Mavs4 <pfragkakis@yahoo.gr>
2015-12-20 11:49:43 +0200

    Update WebBrowser.java
    diwrthwsei sxolia

--- WebBrowser.java
+++ WebBrowser.java
@@ -95,7 +95,7 @@ public class WebBrowser extends JFrame implements ActionListener {
 										printText(tag_1.p(line));
 									}
 								        if(line.contains("<li")){
-//an brei tag li (dId "<li") tote kalei kai aksiopoiei th me8odo p ths Tags_1
+//an brei tag li (dId "<li") tote kalei kai aksiopoiei th me8odo li ths Tags_1
 										printText(tag_1.li(line));
 								        }
 								}
