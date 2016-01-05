707ab8896babab7c8adea5e8c835ce8d29c95a8c
steliosmps <batr25@Yahoo.gr>
2016-01-05 11:35:14 +0200

    Perasmeno apo checkstyle!
    Den mporesa na lusw monos mou ola ta lathi!An kapoios mporei na lusei kapoia pou emeinan na to kanei!

--- URLCon.java
+++ URLCon.java
@@ -1,26 +1,24 @@
 import java.net.*;
 import java.io.*;
-import java.util.*;
 import java.io.File;
 import java.io.BufferedWriter;
 
-
-
 public class URLCon {
-	public URLCon() {};
-    public void createTxt(String string_url) throws Exception {
+	public URLCon() { };
+    public final void createTxt(final String string_url) throws Exception {
 
-	String encoding = "UTF-8"; // Or "ISO-8859-7".h entolh tha xrhsimopoihthei gia thn emfanish twn ellinikwn
+	String encoding = "UTF-8"; // Or "ISO-8859-7".h
+	//entolh tha xrhsimopoihthei gia thn emfanish twn ellinikwn
 
-	String str = string_url;
-
-
-	URL a = new URL("http://"+string_url);
+	URL a = new URL("http://"
+	+ string_url);
 	URLConnection y_c = a.openConnection();
-	BufferedReader in = new BufferedReader(new InputStreamReader( y_c.getInputStream(),encoding)); //To encoding einai gia ta ellinika
+	BufferedReader in = new BufferedReader(new InputStreamReader
+	(y_c.getInputStream(), encoding)); //To encoding einai gia ta ellinika
 
-       	try{
-		File file = new File("html.txt"); //to arxeio ftiaxnetai ston idio fakelo me tis klasseis
+       	try {
+		File file = new File("html.txt"); //to arxeio ftiaxnetai 
+		//sto idio fakelo me tiw klasseis
 
 		FileWriter fw = new FileWriter(file.getAbsoluteFile());
 		BufferedWriter bw = new BufferedWriter(fw);
@@ -28,7 +26,7 @@ public class URLCon {
 		String line = null;
 
 		String inputLine;
-		while ((inputLine = in.readLine()) != null){
+		while ((inputLine = in.readLine()) != null) {
 
 			bw.write(inputLine);
             		bw.newLine();
